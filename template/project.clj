669517f5215251e28c039f8284559eca51e2696c
monjohn <johnston.monte@gmail.com>
2015-06-08 15:10:15 -0400

    datomic working

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,6 @@
 (defproject clojureapp "0.1.1-SNAPSHOT"
     :plugins [[lein-ring "0.9.4"]]
     :dependencies [[org.clojure/clojure "1.6.0"]
-                   [http-kit "2.1.18"]]
+                   [http-kit "2.1.18"]
+                   [com.datomic/datomic-free "0.9.5173" :exclusions [joda-time]]]
     :main webapp)
